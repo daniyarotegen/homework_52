4a05784f71ee1b815696c4096a67038bb3752836
Daniyar Otegen <daniyarotegen@gmail.com>
2023-02-14 11:47:11 +0600

    Added models, index view and index template

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    description = models.TextField(max_length=200, null=False, blank=False, verbose_name='description')
+    status = models.CharField(max_length=10, null=False, blank=False, verbose_name='status', default='new')
+    completion_date = models.DateField(null=True, blank=True, verbose_name='Completion time')
+
+    def __str__(self):
+        return f'{self.description} = {self.status}'
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
+from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
+from todo.models import Task
 
-# Create your views here.
+
+def index_view(request: WSGIRequest):
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'index.html', context=context)
